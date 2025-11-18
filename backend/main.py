b41dd6b04ea1c6df4318963735a5f102816551c7
Aman <amanpal8149@gmail.com>
2025-11-18 14:40:41 +0530

    http-https

--- backend/main.py
+++ backend/main.py
@@ -95,6 +95,7 @@ app.add_middleware(
     allow_origins=[
         "https://icu-ruby.vercel.app",
         "https://icu-git-main-aman-pals-projects-57314315.vercel.app",
+        "https://xpvmw0sxz2.execute-api.us-east-2.amazonaws.com",
         "https://main.d1wagczisbb966.amplifyapp.com",
         "https://icu-1-r21u.onrender.com",
         "http://localhost:8080",
@@ -126,6 +127,7 @@ async def health_check():
             "https://icu-git-main-aman-pals-projects-57314315.vercel.app",
             "https://icu-1-r21u.onrender.com",
             "https://main.d1wagczisbb966.amplifyapp.com",
+            "https://xpvmw0sxz2.execute-api.us-east-2.amazonaws.com",
             "http://localhost:8080",
         ]
     }
