fd0c2c4b03aaf45c90e765dcd70186df7e6533df
Aman <amanpal8149@gmail.com>
2025-11-19 03:33:39 +0530

    server

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,14 @@
-output "alb_dns" {
-  value = aws_lb.app_lb.dns_name
+output "ec2_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.app.public_ip
 }
 
-output "ec2_ip" {
-  value = aws_instance.backend.public_ip
+output "api_gateway_endpoint" {
+  description = "HTTPS endpoint (API Gateway) that forwards to EC2"
+  value       = aws_apigatewayv2_api.httpapi.api_endpoint
 }
 
-output "s3_bucket" {
-  value = aws_s3_bucket.model_bucket.bucket
+output "s3_bucket_name" {
+  description = "S3 bucket name for models"
+  value       = aws_s3_bucket.models.bucket
 }
