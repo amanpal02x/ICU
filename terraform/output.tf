3bf74b2acd785f842d0defdc94eedd0e1efd4731
Aman <amanpal8149@gmail.com>
2025-11-20 16:09:38 +0530

    runtime

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,19 +2,24 @@
 # outputs.tf
 ############################################################
 
-output "api_gateway_invoke_url" {
-  value       = aws_apigatewayv2_stage.default.invoke_url
-  description = "Public HTTPS API endpoint"
-}
-
 output "nlb_dns_name" {
   value       = aws_lb.nlb.dns_name
-  description = "Network Load Balancer DNS"
+  description = "Network Load Balancer DNS (public)"
 }
 
-output "s3_bucket" {
+output "api_gateway_invoke_url" {
+  value       = aws_apigatewayv2_stage.default.invoke_url
+  description = "API Gateway public HTTPS endpoint"
+}
+
+output "s3_bucket_name" {
   value       = aws_s3_bucket.model_bucket.bucket
-  description = "S3 bucket storing your ML models"
+  description = "S3 bucket where model files are stored"
+}
+
+output "ec2_public_ip" {
+  value       = aws_eip.ec2_eip.public_ip
+  description = "Elastic IP assigned to the EC2 instance (public IP)"
 }
 
 output "ec2_private_ip" {
@@ -27,11 +32,7 @@ output "ec2_instance_id" {
   description = "EC2 instance ID"
 }
 
-output "generated_private_key" {
-  value     = tls_private_key.generated.private_key_pem
-  sensitive = true
-}
-
-output "admin_cidr_final" {
-  value = local.admin_cidr_final
+output "uploaded_image_path" {
+  value       = "/mnt/data/5b2c6bec-eda4-453d-82b6-af838bdd8e9d.png"
+  description = "Local uploaded screenshot path (for reference)"
 }
